6f547be3a886fdb2fc163264014b9409f1eee496
Jason Kuhrt <jasonkuhrt@me.com>
2020-01-23 20:12:45 -0500

    link to next steps

--- blog/tests/integration.jest.ts
+++ blog/tests/integration.jest.ts
@@ -2,7 +2,6 @@ import { createTestContext } from 'graphql-santa/testing'
 
 beforeEach(async () => {
   const ctx = await createTestContext()
-  // ctx.app.db.client.
 })
 
-// Refer to https://prisma-labs.github.io/graphql-santa/#/guides/testing?id=with-a-database
+// https://prisma-labs.github.io/graphql-santa/#/guides/testing?id=with-a-database
