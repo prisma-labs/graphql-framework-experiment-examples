b59ae97b79f1a3d69295d8f75885a773594ac53c
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-04-28 19:51:03 +0200

    hardcode serverless

--- nextjs/graphql/query.ts
+++ nextjs/graphql/query.ts
@@ -1,10 +1,9 @@
 import { schema } from "nexus";
-import { NextApiRequest, NextApiResponse } from "next";
+
+console.log("==> query.ts module is loading");
 
 schema.queryType({
   definition(t) {
     t.string("hello", () => "world");
   },
 });
-
-export default (req: NextApiRequest, res: NextApiResponse) => {};
--- nextjs/pages/api/graphql.ts
+++ nextjs/pages/api/graphql.ts
@@ -1,6 +1,34 @@
-import { NextApiRequest, NextApiResponse } from "next";
+process.env.NEXUS_SHOULD_GENERATE_ARTIFACTS = "false";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("TITITITITI");
-  res.json({ hello: "world" });
+import { NextApiRequest, NextApiResponse } from "next";
+import Axios from "axios";
+
+const app = require("nexus").default;
+const { server } = require("nexus");
+
+const axios = Axios.create({
+  baseURL: "http://localhost:4000",
+});
+
+export default async (_req: NextApiRequest, res: NextApiResponse) => {
+  require("../../graphql/query");
+  await server.start();
+
+  const graphqlRes = await axios.post(
+    "/graphql",
+    {
+      query: `{ hello }`,
+      variables: {},
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  await server.stop();
+  app.resetSingleton();
+  require.cache = {};
+  return res.json(graphqlRes.data);
 };
