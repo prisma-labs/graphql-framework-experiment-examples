bf0ab1c6fd379a3fb13c377727c9864a24cd5784
Jason Kuhrt <jasonkuhrt@me.com>
2020-05-20 08:52:11 -0400

    fix(nextjs): dev time assembly

--- with-nextjs/pages/index.tsx
+++ with-nextjs/pages/index.tsx
@@ -8,8 +8,9 @@ export default function Home() {
         <title>Next.js × Nexus Todo App</title>
       </Head>
       <p>
-        The Todo App will be here one day. For now just go open the{' '}
-        <a href="/api/playground">Nexus Playground</a>.
+        The Todo App will be here one day. For now get{' '}
+        <a href="https://www.electronjs.org/apps/graphql-playground">GraphQL PLayground</a> and point it at
+        `/api/graphql`.
       </p>
     </Layout>
   )
