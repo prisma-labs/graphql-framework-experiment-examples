a825d5d6f69c3f8a6398daa957406b4a6681f500
Jason Kuhrt <jasonkuhrt@me.com>
2020-07-11 21:40:23 -0400

    wip

---
+++ hello-world/api/test.ts
@@ -0,0 +1,37 @@
+import { createTestContext, TestContext } from "nexus/testing"
+
+let ctx = {} as TestContext
+
+function send(strings: TemplateStringsArray) {
+  const document = strings.join("")
+  return ctx.client.send(document)
+}
+
+beforeAll(async () => {
+  const testContext = await createTestContext()
+  Object.assign(ctx, testContext)
+  await ctx.app.start()
+})
+
+afterAll(async () => {
+  await ctx.app.stop()
+})
+
+it("works", async () => {
+  const result = await send`
+    query {
+      users {
+        name
+      }
+    }
+  `
+  expect(result).toMatchInlineSnapshot(`
+    Object {
+      "users": Array [
+        Object {
+          "id": "1",
+        },
+      ],
+    }
+  `)
+})
