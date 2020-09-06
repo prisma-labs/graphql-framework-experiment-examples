5942229384af663406189694e66d9a2ab2aac651
Jason Kuhrt <jasonkuhrt@me.com>
2020-09-05 23:17:23 -0400

    feat: show off working subscriptions

--- hello-world/api/graphql/user.ts
+++ hello-world/api/graphql/user.ts
@@ -1,4 +1,4 @@
-import { schema, settings } from "nexus"
+import { schema } from "nexus"
 
 schema.objectType({
   name: "User",
@@ -25,22 +25,15 @@ schema.objectType({
   },
 })
 
-async function* truthStream() {
-  while (true) {
-    const answer = [true, false][Math.round(Math.random())]
-    yield answer
-    await new Promise((res) => setTimeout(res, 1000))
-  }
-}
-
 schema.subscriptionType({
   definition(t) {
-    // todo t.boolean seems broken
+    // todo Nexus Schema t.boolean is broken
     t.field("truths", {
       type: "Boolean",
       subscribe() {
         return truthStream()
       },
+      // todo Nexus Schema doesn't infer type currently
       resolve(answer: boolean) {
         return answer
       },
@@ -48,8 +41,10 @@ schema.subscriptionType({
   },
 })
 
-settings.change({
-  server: {
-    playground: undefined,
-  },
-})
+async function* truthStream() {
+  while (true) {
+    const answer = [true, false][Math.round(Math.random())]
+    yield answer
+    await new Promise((res) => setTimeout(res, 1000))
+  }
+}
